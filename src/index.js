61d677da548fc0f29c1fb9a0f03a952464172f20
That-1-JS_Nerd <itsNathan022@gmail.com>
2022-04-22 19:56:27 +0200

    new commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql
+} from '@apollo/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new ApolloClient({
+    uri: 'https://48p1r2roz4.sse.codesandbox.io', // Specifies url of GraphQL server
+    cache: new InMemoryCache() // Caches query results after fetching
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render(<React.StrictMode>
+    <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
